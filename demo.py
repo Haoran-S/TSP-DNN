d006afd45e6cb31e87bf9fa813f90405453b484c
Haoran Sun <Sunhr1993@gmail.com>
2021-02-04 22:47:06 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,8 +8,9 @@
 #  Codes have been tested successfully on Python 3.6.0 with TensorFlow 1.0.0 and Numpy 1.12.0 support.
 #
 #  References: 
-#   [1] Haoran Sun, Xiangyi Chen, Qingjiang Shi, Mingyi Hong, Xiao Fu, and Nikos D. Sidiropoulos. 
-#       "LEARNING TO OPTIMIZE: TRAINING DEEP NEURAL NETWORKS FOR WIRELESS RESOURCE MANAGEMENT."
+#   [1] Haoran Sun, Xiangyi Chen, Qingjiang Shi, Mingyi Hong, Xiao Fu, and Nikos D. Sidiropoulos, 
+#       "Learning to Optimize: Training Deep Neural Networks for Interference Management," 
+#       in IEEE Transactions on Signal Processing, vol. 66, no. 20, pp. 5438-5453, 15 Oct.15, 2018.
 #
 #  version 1.0 -- February 2017
 #  Written by Haoran Sun (hrsun AT iastate.edu)
